498333dd0e238052cab1eccdf40a9b134057766e
Jonathon Leight <jonathon.leight@jleight.com>
2023-02-11 09:05:30 -0500

    Add console app to start testing with

---
+++ samples/Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Leightweight.Multitenancy.Abstractions;
+
+var user = new User(Guid.NewGuid(), "jleight");
+var wrapper = new TenantWrapper<User> { Tenant = user };
+var tenant = (ITenant<User>)wrapper;
+
+Console.WriteLine($"ID: {tenant.Tenant?.Id}");
+Console.WriteLine($"Username: {tenant.Tenant?.Username}");
+
+file record User(Guid Id, string Username);
